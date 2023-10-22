3f79538d4f3d9f7695de5b8ed48ab4da508cb286
wandersonxc <wandersonxcarvalho@gmail.com>
2023-10-22 10:53:54 -0300

    finished project

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,29 +1,30 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "Marvel anuncia um novo filme do Homem de Ferro, confira",
-    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Desenvolvimento Web com Angular",
+    "description": " Aprofunde-se no emocionante mundo do desenvolvimento web com o Angular, um dos frameworks mais populares para a criação de aplicações web interativas e dinâmicas. Descubra como utilizar o Angular para desenvolver aplicativos da web de alto desempenho e com uma experiência do usuário excepcional. Este tópico abordará desde conceitos fundamentais até técnicas avançadas de desenvolvimento com Angular, permitindo que você crie projetos web modernos e atraentes. Este tópico de destaque é ideal para aqueles que desejam dominar uma das tecnologias mais relevantes e empolgantes no desenvolvimento web atual. Ele oferece uma visão geral do conteúdo que será explorado em profundidade no blog TechHub Blog, com foco no Angular.",
+    "photoCover": "https://hermes.dio.me/tracks/5a199bba-a494-49ce-b357-f746eb7b7425.png"
   },
 
   {
     "id":"2",
-    "title": "Nova série anunciada no Disney+",
-    "description": "ola",
-    "photoCover": "https://nosbastidores.com.br/wp-content/uploads/2021/08/Series-Marvel-Disney-Plus-1024x576-1.jpg"
+    "title": "HTML, CSS e JavaScript para Iniciantes",
+    "description": "Dominar os fundamentos da web é o primeiro passo para se tornar um desenvolvedor de sucesso. Neste tópico, você mergulhará nos três pilares da web: HTML, CSS e JavaScript. Aprenda como usar o HTML para criar a estrutura das suas páginas, o CSS para torná-las visualmente atraentes e o JavaScript para torná-las interativas. Este é o ponto de partida ideal para quem está começando no mundo do desenvolvimento web.",
+    "description1": "Introdução ao HTML: A base da web. Aprenda como o HTML é usado para estruturar o conteúdo de páginas web e criar elementos como textos, links e imagens.",
+    "photoCover": "https://hermes.dio.me/articles/cover/9706f214-6e23-4198-baa8-fcdd03589577.jpg"
   },
 
   {
     "id":"3",
-    "title": "Novo filme do Pantera Negra será lançado em breve",
-    "description": "ola",
+    "title": "Segurança da Informação na Web",
+    "description": "A segurança é uma prioridade absoluta no mundo digital. Neste tópico, exploraremos as práticas essenciais de segurança da informação no desenvolvimento web. Aprenda como proteger seu site contra ameaças cibernéticas, como hackers e ataques maliciosos. Abordaremos temas como criptografia para proteger dados sensíveis, autenticação segura e estratégias para manter sua aplicação web segura. Sua jornada rumo a um desenvolvimento web seguro começa aqui.",
     "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F6482D58C8BB4D899FA4682BF27652C3E1BB73F5971E5F963994112F26A73690/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
 
   {
     "id":"4",
-    "title": "Novo filme dos X-Men está em pós produção, afirma diretor",
-    "description": "ola",
-    "photoCover": "https://images.bauerhosting.com/legacy/empire-tmdb/films/127585/images/ai9UKd8KownQKGIh1m5p3DuEeMm.jpg?ar=16%3A9&fit=crop&crop=top&auto=format&w=1440&q=80"
+    "title": "Redes de Computadores e Desenvolvimento de Sistemas Web",
+    "description": "O mundo da tecnologia está interligado por redes de computadores. Neste tópico, exploraremos como as redes afetam o desenvolvimento de sistemas web. Aprenda a criar aplicativos e sistemas web que funcionem de maneira eficaz em ambientes de rede, compreendendo como os dados são transmitidos e recebidos. Descubra como integrar seus projetos de desenvolvimento web com a infraestrutura de rede para criar experiências digitais perfeitas.",
+    "photoCover": "https://hermes.dio.me/articles/cover/255bc27d-3d67-4aeb-b53f-a033c4af3311.jpg"
   }
 ]
